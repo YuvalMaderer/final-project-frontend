e83a321a2a3b97db641354679d4dee7988be8369
Joni Uzan <joniuzan33@gmail.com>
2024-09-08 09:34:21 +0300

    lll

--- src/components/general-components/HomeDetails.tsx
+++ src/components/general-components/HomeDetails.tsx
@@ -84,8 +84,8 @@ function HomeDetails() {
   const hostId = home?.host._id;
 
   const position: { lat: number; lng: number } = {
-    lat: home!.loc.lat,
-    lng: home!.loc.lan,
+    lat: home?.loc.lat,
+    lng: home?.loc.lan,
   };
   const getReservedDates = (reservations: IReservation[]): Date[] => {
     return reservations.flatMap((reservation) => {
